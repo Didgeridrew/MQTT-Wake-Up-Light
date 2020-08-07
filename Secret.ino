ee9f2de025b078a189fc1b4ab06549d44434c33e
Didgeridrew <didgeridrew@hotmail.com>
2020-08-06 21:14:57 -0400

    Add files via upload
    Update to jLed non-blocking led effects library to fix issues with fade sequence

---
+++ Secret.ino
@@ -0,0 +1,14 @@
+/*Place sensitive information such as WiFi SSIDs, paswords, usernames etc here 
+
+
+*/
+
+const char* ssid = "";                       // Wifi SSID
+const char* wifiPassword = "";        // Wifi Password, if required.
+
+const char* mqttServerName = "";
+const char* username = "";                       
+const char* mqttPassword = "";
+
+const char* otaUser = "";
+const char* otaPassword = "";
